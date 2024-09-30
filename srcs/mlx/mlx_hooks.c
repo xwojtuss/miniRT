08fa19ddeac299086c6dd700536f4b20649f4850
xwojtuss <wojkor338@gmail.com>
2024-09-30 19:03:46 +0200

    added movement, mouse movement started

--- srcs/mlx/mlx_hooks.c
+++ srcs/mlx/mlx_hooks.c
@@ -6,11 +6,21 @@
 /*   By: wkornato <wkornato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:40:25 by wkornato          #+#    #+#             */
-/*   Updated: 2024/09/24 15:40:26 by wkornato         ###   ########.fr       */
+/*   Updated: 2024/09/30 18:44:51 by wkornato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
+
+int	mouse_hook(int x, int y, t_scene *scene)
+{
+	(void)x;
+	(void)y;
+	(void)scene;
+	// change cam orientation here
+	mlx_mouse_move(scene->mlx, scene->win, scene->win_width / 2, scene->win_height / 2);
+	return (EXIT_SUCCESS);
+}
 
 int	key_hook(int keycode, t_scene *scene)
 {
@@ -20,7 +30,28 @@ int	key_hook(int keycode, t_scene *scene)
 		free_scene(scene);
 		exit(EXIT_SUCCESS);
 	}
-	return (0);
+	else if (keycode == XK_w)
+		scene->camera->position.z -= 0.5;
+	else if (keycode == XK_s)
+		scene->camera->position.z += 0.5;
+	else if (keycode == XK_a)
+		scene->camera->position.x -= 0.5;
+	else if (keycode == XK_d)
+		scene->camera->position.x += 0.5;
+	else if (keycode == XK_space)
+		scene->camera->position.y += 0.5;
+	else if (keycode == XK_c)
+		scene->camera->position.y -= 0.5;
+	else if (keycode == XK_equal)
+		scene->camera->fov += 5;
+	else if (keycode == XK_minus)
+		scene->camera->fov -= 5;
+	else
+		return (EXIT_SUCCESS);
+	print_camera_parameters(scene->camera);
+	initialize_viewport(scene);
+	render_scene(scene);
+	return (EXIT_SUCCESS);
 }
 
 int	close_win_handler(void *context)
