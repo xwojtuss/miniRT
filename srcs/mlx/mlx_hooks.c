0715345e2bad02a8a5e7371dea1c38cce0a24629
xwojtuss <wojkor338@gmail.com>
2024-10-01 15:21:21 +0200

    fixed fov

--- srcs/mlx/mlx_hooks.c
+++ srcs/mlx/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: wkornato <wkornato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:40:25 by wkornato          #+#    #+#             */
-/*   Updated: 2024/09/30 18:44:51 by wkornato         ###   ########.fr       */
+/*   Updated: 2024/10/01 15:17:35 by wkornato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,9 @@ int	key_hook(int keycode, t_scene *scene)
 		scene->camera->position.y += 0.5;
 	else if (keycode == XK_c)
 		scene->camera->position.y -= 0.5;
-	else if (keycode == XK_equal)
+	else if (keycode == XK_equal && scene->camera->fov < 175)
 		scene->camera->fov += 5;
-	else if (keycode == XK_minus)
+	else if (keycode == XK_minus && scene->camera->fov > 5)
 		scene->camera->fov -= 5;
 	else
 		return (EXIT_SUCCESS);
