16ad4cbe7f0347b490e0d0ea7658b22d6e59d42b
Wojtek <wojkor338@gmail.com>
2024-11-16 02:47:04 +0100

    normal for plane, how tf to implement phong?!

--- srcs/raytracing/normal_vectors.c
+++ srcs/raytracing/normal_vectors.c
@@ -4,3 +4,10 @@ t_vector	get_normal_vector_sphere(t_ray ray, t_vector center)
 {
 	return (normalize_vector(subtract_v(ray.origin, center)));
 }
+
+t_vector	get_normal_vector_plane(t_ray ray, t_plane plane)
+{
+	if (dot_product(subtract_v(ray.origin, plane.position), plane.orientation) > 0)
+		return (multiply_v(plane.orientation, -1));
+	return (plane.orientation);
+}
--- srcs/raytracing/phong.c
+++ srcs/raytracing/phong.c
@@ -6,5 +6,19 @@ returns an integer value of the color
 */
 int	phong_reflection(t_vector normal_vector, t_vector ray_direction, t_scene scene)
 {
-	
+	/*
+    
+    The wikipedia article specifies the light as only one value
+    but in our program it has a brightness and a colour (i think as bonus)
+    how would we calculate it?
+    we need the direction to all the lights
+    we need the normals, they are passed as parameters
+    we need the value for the lights (again, single value from brightness and colour)
+    we need the perfectly reflected ray from light = 2(intersection point to lights * normal)normal - intersection point to lights
+    we need the direction to the camera
+    shininess of material as a constant or a map of some sorts
+    jesus the article is not that readable
+    it makes sense but only in my mind, how tf to implement it ?!?1
+    
+    */
 }
