eeb1f57c397b7513afec533d087271acebccb28d
xwojtuss <wojkor338@gmail.com>
2024-11-16 02:47:04 +0100

    normal for plane, how tf to implement phong?!

--- includes/phong_reflection.h
+++ includes/phong_reflection.h
@@ -1,6 +1,15 @@
 #ifndef PHONG_REFLECTION_H
 # define PHONG_REFLECTION_H
 
+typedef struct s_raytrace_info
+{
+    t_vector    intersection_point;
+    t_vector    normal_vector;
+    t_vector    ray_direction;
+    t_scene     scene;
+}               t_raytrace_info;
+
+
 # define SPECULAR_CONST
 # define DIFFUSE_CONST
 # define AMBIENT_CONST
